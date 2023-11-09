0f4184b8d1c66f7eb4b82e2092fe273740312124
kTurek95 <k.turek1995@gmail.com>
2023-11-09 08:17:29 +0000

    added new apps + django_extensions to installed_apps

--- CurrencyExchange/settings.py
+++ CurrencyExchange/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions',
+
+    'CurrencyPairs.apps.CurrencypairsConfig',
+    'AvailableCurrencies.apps.AvailablecurrenciesConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SHELL_PLUS_PRINT_SQL = True
