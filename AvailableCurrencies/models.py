e81db1f45949b64dc644bd10fe5a5cbdf7806786
kTurek95 <k.turek1995@gmail.com>
2023-11-10 10:54:24 +0000

    updated AvailableCurrency model and created new CurrencyExchangeRate model

--- AvailableCurrencies/models.py
+++ AvailableCurrencies/models.py
@@ -3,8 +3,17 @@ from django.contrib.auth.models import User
 
 
 class AvailableCurrency(models.Model):
-    name = models.CharField(max_length=255)
-    code = models.CharField(max_length=4)
+    name = models.CharField(max_length=255, null=True)
+    code = models.CharField(max_length=5)
+    country_code = models.CharField(max_length=5, null=True)
+    country_name = models.CharField(max_length=255, null=True)
+    status = models.CharField(max_length=255)
 
     def __str__(self):
-        return f'{self.code} - {self.name} '
+        return f'{self.code} - {self.name}'
+
+
+class CurrencyExchangeRate(models.Model):
+    currency = models.ForeignKey(AvailableCurrency, on_delete=models.CASCADE)
+    rate_to_usd = models.CharField(max_length=255)
+    api_date_updated = models.DateField()
