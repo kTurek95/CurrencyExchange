16d34848dde107e9b8f6968cb16d0e49f7f58bf6
kTurek95 <k.turek1995@gmail.com>
2023-11-10 23:29:36 +0000

    updated path in urlpatterns in AvailableCurrencies app

--- AvailableCurrencies/urls.py
+++ AvailableCurrencies/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import currencies, currencies_list
+from .views import currencies, currencies_list, currencies_details
+
+app_name = 'currencies'
 
 urlpatterns = [
-    path('', currencies),
-    path('list/', currencies_list)
+    path('test', currencies),
+    path('', currencies_list, name='list'),
+    path('<int:currency_id>', currencies_details, name='details')
 ]
