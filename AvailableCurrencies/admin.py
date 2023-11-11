ff1216e47ee38331d48f968dd29bd674db52b9d4
kTurek95 <k.turek1995@gmail.com>
2023-11-11 20:12:56 +0000

    created AvailableCurrenciesAdmin class in the admin module of the AvailableCurrencies app

--- AvailableCurrencies/admin.py
+++ AvailableCurrencies/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import AvailableCurrency
 
-admin.site.register(AvailableCurrency)
+
+class AvailableCurrenciesAdmin(admin.ModelAdmin):
+    list_display = ['code', 'name']
+    search_fields = ['code']
+    list_filter = ['code']
+
+
+admin.site.register(AvailableCurrency, AvailableCurrenciesAdmin)
