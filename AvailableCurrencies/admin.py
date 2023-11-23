0886db7ed82364db0ffde24d4a789959b5beddb7
kTurek95 <k.turek1995@gmail.com>
2023-11-23 23:02:39 +0000

    Enhancement of AvailableCurrenciesAdmin view, Added CurrencyResource and CurrencyExchangeRateAdmin classes

--- AvailableCurrencies/admin.py
+++ AvailableCurrencies/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
-from .models import AvailableCurrency
+from import_export import resources
+from import_export.admin import ExportMixin
+from .models import AvailableCurrency, CurrencyExchangeRate
 
 
-class AvailableCurrenciesAdmin(admin.ModelAdmin):
-    list_display = ['code', 'name']
+class CurrencyResource(resources.ModelResource):
+    class Meta:
+        model = AvailableCurrency
+
+
+class AvailableCurrenciesAdmin(ExportMixin, admin.ModelAdmin):
+    list_display = ['code', 'name', 'country_name']
     search_fields = ['code']
-    list_filter = ['code']
+    resource_class = CurrencyResource
+    list_filter = ['country_name']
 
 
 admin.site.register(AvailableCurrency, AvailableCurrenciesAdmin)
+
+
+class CurrencyExchangeRateAdmin(admin.ModelAdmin):
+    list_display = ['currency_id', 'rate_to_usd']
+
+
+admin.site.register(CurrencyExchangeRate, CurrencyExchangeRateAdmin)
