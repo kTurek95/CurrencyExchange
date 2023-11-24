cd6765fc7b4755067986bcc60ffd02d2eb150cf0
kTurek95 <k.turek1995@gmail.com>
2023-11-24 23:31:30 +0000

    created and registered UserProfileAdmin class in main app

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from main.models import UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
