e1507ce9c8e5c3c494ca0969a1164717edf401bd
kTurek95 <k.turek1995@gmail.com>
2023-11-25 23:21:07 +0000

    created and registered CurrencyPairsAdmin class

--- CurrencyPairs/admin.py
+++ CurrencyPairs/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import CurrencyPair
 
-admin.site.register(CurrencyPair)
+
+class CurrencyPairsAdmin(admin.ModelAdmin):
+    list_display = ['code', 'name']
+    search_fields = ['code']
+    list_filter = ['code']
+
+
+admin.site.register(CurrencyPair, CurrencyPairsAdmin)
