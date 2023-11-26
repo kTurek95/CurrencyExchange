198d324687fbfe699901d03cb4dac9df24f2a621
kTurek95 <k.turek1995@gmail.com>
2023-11-26 20:34:34 +0000

    added login viev function to views module in AvailableCurrencies app

--- AvailableCurrencies/urls.py
+++ AvailableCurrencies/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import currencies, currencies_list, currencies_details
+from .views import currencies, currencies_list, currencies_details, currencies_rate
 
 app_name = 'currencies'
 
 urlpatterns = [
     path('test', currencies),
-    path('', currencies_list, name='list'),
-    path('<int:currency_id>', currencies_details, name='details')
+    path('list/', currencies_list, name='list'),
+    path('<int:currency_id>', currencies_details, name='details'),
+    path('rate/', currencies_rate, name='rate')
 ]
