648b315341c301664c1a631a553e46f21ed9163c
kTurek95 <k.turek1995@gmail.com>
2023-11-26 08:41:02 +0000

    added the option to display a chart using matplotlib to the function currencies_details and updated currencies_rate function

--- AvailableCurrencies/views.py
+++ AvailableCurrencies/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.template import loader
+from django.db.models import Max
 from django.http import HttpResponse
 from django.core.paginator import Paginator
-import json
-import requests
+from datetime import datetime, timedelta
+from matplotlib import pyplot as plt
+import matplotlib
+import matplotlib.dates as mdates
 from .models import AvailableCurrency, CurrencyExchangeRate
 
 
@@ -20,44 +22,67 @@ def currencies_list(request):
     return render(request, 'AvailableCurrencies/currencies_list.html', context)
 
 
-def get_currencies_symbol_from_api(request):
-    params = {
-        'apikey': 'e0ad4966bceb4346b7949c5e3d5a6dee',
-    }
-    r = requests.get('https://api.currencyfreaks.com/v2.0/supported-currencies', params)
-
-    try:
-        content = r.json()
-    except json.JSONDecodeError:
-        return HttpResponse('Wrong format')
-    else:
-        for currency_key in content["supportedCurrenciesMap"]:
-            currency_data = content["supportedCurrenciesMap"][currency_key]
-
-            status_bool = currency_data['status'] == 'AVAILABLE'
-
-            AvailableCurrency.objects.update_or_create(
-                code=currency_data['currencyCode'],
-                defaults={
-                    'name': currency_data['currencyName'],
-                    'country_code': currency_data.get('countryCode', ''),
-                    'country_name': currency_data.get('countryName', ''),
-                    'status': status_bool,
-                }
-            )
-
-
 def currencies_details(request, currency_id: int):
+    matplotlib.use('Agg')
+    five_days_ago = datetime.now() - timedelta(days=5)
+
+    dates = []
+    currency_rates = []
+
     currency = AvailableCurrency.objects.get(pk=currency_id)
     rate = CurrencyExchangeRate.objects.filter(currency=currency).first()
+
+    currencies = AvailableCurrency.objects.filter(id=currency_id)
+    rates = CurrencyExchangeRate.objects.filter(
+        currency_id=currencies.first(),
+        api_date_updated__gt=five_days_ago
+    )
+    for rate in rates:
+        dates.append(rate.api_date_updated)
+        currency_rates.append(rate.rate_to_usd)
+
+    data_pairs = sorted(zip(dates, currency_rates), key=lambda x: x[1])
+    sorted_dates, sorted_currency_rates = zip(*data_pairs)
+
+    sorted_dates = [datetime.strptime(date, '%Y-%m-%d') if isinstance(date, str) else date for date in sorted_dates]
+    sorted_currency_rates = [float(rate) for rate in sorted_currency_rates]
+
+    plt.figure(figsize=(10, 5))
+
+    plt.bar(sorted_dates, sorted_currency_rates)
+
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+
+    plt.ylim(min(sorted_currency_rates) - 1, max(sorted_currency_rates) + 1)
+
+    plt.title(currencies.first())
+    plt.xlabel('Dates')
+    plt.ylabel('Currency rates')
+
+    plt.xticks(rotation=45)
+
+    plt.tight_layout()
+    plt.grid(True)
+
+    image_path = f'AvailableCurrencies/static/AvailableCurrencies/img/chart_{currency_id}.png'
+    plt.savefig(image_path)
+    plt.close()
+
     context = {'currency_details': currency,
-               'rates': rate}
+               'rates': rate,
+               'image_name': f'chart_{currency_id}.png'
+               }
+
     return render(request, 'AvailableCurrencies/currencies_details.html', context)
 
 
 def currencies_rate(request):
-    rate = CurrencyExchangeRate.objects.all().order_by('currency__code')
-    paginator = Paginator(rate,30)
+    latest_rates = CurrencyExchangeRate.objects.values('currency_id').annotate(latest_date=Max('api_date_updated'))
+    rate = CurrencyExchangeRate.objects.filter(
+        api_date_updated__in=[item['latest_date'] for item in latest_rates]
+    ).order_by('currency__code')
+    paginator = Paginator(rate, 30)
     page_number = request.GET.get('page')
     rates_list = paginator.get_page(page_number)
     context = {'currencies_rate': rates_list}
