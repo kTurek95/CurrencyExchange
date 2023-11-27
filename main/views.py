0de076045f8a3c8fbf9494942fb8d15138b01ab0
kTurek95 <k.turek1995@gmail.com>
2023-11-27 16:49:28 +0000

    updated and rename hello_world function to currency_view

--- main/views.py
+++ main/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from AvailableCurrencies.models import AvailableCurrency, CurrencyExchangeRate
 
 
-def hello_word(request):
-    return HttpResponse('Hello World')
+def currency_view(request):
+    result = None
+    currency = AvailableCurrency.objects.all().order_by('code')
+    rate = None
+    
+    if request.method == 'POST':
+        selected_currency = request.POST.get('currency')
+        amount = float(request.POST.get('amount'))
+        
+        rate = CurrencyExchangeRate.objects.filter(currency__code=selected_currency).first()
+        result = round(float(rate.rate_to_usd) * amount, 2)
+
+    context = {
+        'currency_view': currency,
+        'result': result,
+        'currency_rate': round(float(rate.rate_to_usd), 2) if rate else None
+    }
+    return render(request, 'main/about.html', context)
