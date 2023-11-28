ed9922c4cd3fb3d90158dd012029557af6ebdfcb
kTurek95 <k.turek1995@gmail.com>
2023-11-28 19:29:39 +0000

    updated view name in path

--- register/urls.py
+++ register/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from main.views import hello_word
+from main.views import currency_view
 from .views import register
 
 app_name = 'register'
 
 urlpatterns = [
     path('register/', register, name='register'),
-    path('', hello_word, name='home')
+    path('', currency_view, name='home')
 ]
