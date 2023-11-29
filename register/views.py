51754b7871707862644109a9abedc60afab14964
kTurek95 <k.turek1995@gmail.com>
2023-11-29 20:14:19 +0000

    created register function for register app in views module

---
+++ register/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from .forms import RegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('register:home'))
+    else:
+        form = RegisterForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
