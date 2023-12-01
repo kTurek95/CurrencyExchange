1b7e03c9c4d7650dcb6f5826b027d3713cae1615
kTurek95 <k.turek1995@gmail.com>
2023-12-01 21:53:51 +0000

    created CryptoTokenCurrency class in module models for CryptoCurrencies app

--- CryptoCurrencies/models.py
+++ CryptoCurrencies/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class CryptoTokenCurrency(models.Model):
+    code = models.CharField(max_length=6)
+    name = models.CharField(max_length=50, null=False)
+    description = models.CharField(max_length=255, null=True)
