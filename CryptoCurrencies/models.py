83c6da93b2e35765f795c49cc256aee316105024
kTurek95 <k.turek1995@gmail.com>
2023-12-05 13:19:26 +0000

    added str method for CryptoTokenCurrency model and created CrytpoTokenRate model in models module for CryptoCurrencies app

--- CryptoCurrencies/models.py
+++ CryptoCurrencies/models.py
@@ -5,3 +5,12 @@ class CryptoTokenCurrency(models.Model):
     code = models.CharField(max_length=6)
     name = models.CharField(max_length=50, null=False)
     description = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.code
+
+
+class CryptoTokenRate(models.Model):
+    token = models.ForeignKey(CryptoTokenCurrency, on_delete=models.CASCADE)
+    rate_to_usd = models.CharField(max_length=10)
+    date = models.DateField()
