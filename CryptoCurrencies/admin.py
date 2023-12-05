199a61b51ae295651b39ba688756f40fcef9817c
kTurek95 <k.turek1995@gmail.com>
2023-12-05 13:13:53 +0000

    created and registered CryptoCurrenciesAdmin class in admin module for CryptoCurrencies app

--- CryptoCurrencies/admin.py
+++ CryptoCurrencies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CryptoTokenCurrency
 
-# Register your models here.
+
+class CryptoCurrenciesAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(CryptoTokenCurrency, CryptoCurrenciesAdmin)
