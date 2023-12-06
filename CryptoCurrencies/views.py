32114ea5ce57a4cbaed3c908699f5ad5039b17f7
kTurek95 <k.turek1995@gmail.com>
2023-12-06 11:09:18 +0000

    created crypto_token view in views module for CryptoCurrencies app

--- CryptoCurrencies/views.py
+++ CryptoCurrencies/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.core.paginator import Paginator
+from .models import CryptoTokenCurrency
 
-# Create your views here.
+
+def crypto_token(request):
+    crypto = CryptoTokenCurrency.objects.all()
+    paginator = Paginator(crypto, 400)
+    page_number = request.GET.get('page')
+    crypto_token = paginator.get_page(page_number)
+    context = {'crypto_token': crypto_token}
+    return render(request, 'CryptoCurrencies/crypto-token-list.html', context)
