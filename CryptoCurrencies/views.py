f1a58b3080decbe7d5340b8e5fcb848406598d62
kTurek95 <k.turek1995@gmail.com>
2023-12-07 13:16:28 +0000

    changed number of pages persite in crypto_token function and added crypto_token_details function in views module for Cryptocurrencies app

--- CryptoCurrencies/views.py
+++ CryptoCurrencies/views.py
@@ -5,8 +5,15 @@ from .models import CryptoTokenCurrency
 
 def crypto_token(request):
     crypto = CryptoTokenCurrency.objects.all()
-    paginator = Paginator(crypto, 400)
+    paginator = Paginator(crypto, 300)
     page_number = request.GET.get('page')
     crypto_token = paginator.get_page(page_number)
     context = {'crypto_token': crypto_token}
     return render(request, 'CryptoCurrencies/crypto-token-list.html', context)
+
+
+def crypto_token_details(request, crypto_id: int):
+    crypto_details = CryptoTokenCurrency.objects.get(pk=crypto_id)
+    context = {'crypto_token_details': crypto_details}
+
+    return render(request, 'CryptoCurrencies/crypto-token-details.html', context)
