0b0a9cbf2f07ae4783e2ea58c6c7ff6808d404a1
kTurek95 <k.turek1995@gmail.com>
2023-12-19 07:35:23 +0000

    Created a static method 'scientific_notation_number' to convert a number in scientific notation (e.g., 1e-03) to a regular number.

--- CryptoCurrencies/models.py
+++ CryptoCurrencies/models.py
@@ -14,3 +14,16 @@ class CryptoTokenRate(models.Model):
     token = models.ForeignKey(CryptoTokenCurrency, on_delete=models.CASCADE)
     rate_to_usd = models.CharField(max_length=10)
     date = models.DateField()
+
+    @staticmethod
+    def scientific_notation_number(mantissa, exponent):
+        return mantissa * (10 ** exponent)
+
+    def __str__(self):
+        if 'e' in self.rate_to_usd:
+            number = self.rate_to_usd.split('-')
+            result = self.scientific_notation_number(float(number[0].split('e')[0]), -1 * int(number[1]))
+            return f'{self.token.name}: {round(result, 6)}'
+        else:
+            return self.token.name + ': ' + self.rate_to_usd + '$'
+
