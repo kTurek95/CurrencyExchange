46b9779e7d54ec21e7d684d18b30ec5c34fb8448
kTurek95 <k.turek1995@gmail.com>
2024-01-29 22:55:30 +0000

    Created the CryptoTokenRate model in the admin module and added the list_display option

--- CryptoCurrencies/admin.py
+++ CryptoCurrencies/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import CryptoTokenCurrency
+from .models import CryptoTokenCurrency, CryptoTokenRate
 
 
 class CryptoCurrenciesAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['code', 'name']
 
 
 admin.site.register(CryptoTokenCurrency, CryptoCurrenciesAdmin)
+
+
+class CryptoTokenRateAdmin(admin.ModelAdmin):
+    list_display = ['token', 'rate_to_usd']
+
+
+admin.site.register(CryptoTokenRate, CryptoTokenRateAdmin)
