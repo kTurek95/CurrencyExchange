be05a9b941820368bd57ace458c008ddf5fb4eac
kTurek95 <k.turek1995@gmail.com>
2024-02-15 22:42:01 +0000

    Added Command class for database cleanup, retaining records from the last 5 days

---
+++ CryptoCurrencies/management/commands/clean_up_old_records.py
@@ -0,0 +1,31 @@
+"""
+Custom Django management command to clean up old records from the CryptoTokenRate model.
+"""
+
+from datetime import timedelta
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+# pylint: disable=import-error
+from CryptoCurrencies.models import CryptoTokenRate
+
+
+class Command(BaseCommand):
+    """
+    This command is designed to be run regularly (e.g., daily) to maintain the database
+    by deleting CryptoTokenRate records that are older than 5 days. It helps in keeping
+    the database size manageable and removing outdated data that is no longer relevant
+    for the application's operation.
+    """
+    help = 'Deletes records older than 5 days'
+
+    # pylint: disable=unused-argument
+    def handle(self, *args, **kwargs):
+        """
+        Execute the command to delete old records from the CryptoTokenRate model.
+
+        It calculates the threshold date (5 days before the current date and time) and
+        deletes all records in the CryptoTokenRate model that are older than this date.
+        """
+        threshold_date = timezone.now() - timedelta(days=5)
+        CryptoTokenRate.objects.filter(date__lt=threshold_date).delete()
+        self.stdout.write('Successfully deleted records older than 5 days.')
