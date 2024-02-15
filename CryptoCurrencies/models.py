8914637cc0e20f27fcaf81a23e0aceb498a23ae4
kTurek95 <k.turek1995@gmail.com>
2024-02-15 09:40:29 +0000

    Update models and migrations

--- CryptoCurrencies/models.py
+++ CryptoCurrencies/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class CryptoTokenCurrency(models.Model):
     code = models.CharField(max_length=6)
     name = models.CharField(max_length=50, null=False)
-    description = models.CharField(max_length=1000, null=True)
+    description = models.CharField(max_length=2000, null=True)
 
     def __str__(self):
         return self.code
