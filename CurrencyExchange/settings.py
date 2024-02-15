61c1f87839d22d2943708cf396979e9c6053422a
kTurek95 <k.turek1995@gmail.com>
2024-02-15 08:15:54 +0000

    updated secret key for more security

--- CurrencyExchange/settings.py
+++ CurrencyExchange/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 import django_heroku
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s2ii%c!1)r9irm)ui7qx1+^ly2m($rw7#0aa0kmwbx(ae+@3tn'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
