09059fb0ce8d3cb0e8a6279d2367c4edd0519325
kTurek95 <k.turek1995@gmail.com>
2024-02-16 10:33:45 +0000

    Added CurrencyExchangeRate model

--- CryptoCurrencies/management/commands/clean_up_old_records.py
+++ CryptoCurrencies/management/commands/clean_up_old_records.py
@@ -7,6 +7,7 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 # pylint: disable=import-error
 from CryptoCurrencies.models import CryptoTokenRate
+from AvailableCurrencies.models import CurrencyExchangeRate
 
 
 class Command(BaseCommand):
@@ -28,4 +29,5 @@ class Command(BaseCommand):
         """
         threshold_date = timezone.now() - timedelta(days=5)
         CryptoTokenRate.objects.filter(date__lt=threshold_date).delete()
+        CurrencyExchangeRate.objects.filter(api_date_updated__lt=threshold_date).delete()
         self.stdout.write('Successfully deleted records older than 5 days.')
