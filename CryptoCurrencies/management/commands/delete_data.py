fe0b47e588808e50d70729cb26a22b29eaa343ff
kTurek95 <k.turek1995@gmail.com>
2024-02-20 04:51:59 +0000

    refactoring code, added option for delete records from sqlite or postgresql database with Id sequence reset

--- CryptoCurrencies/management/commands/delete_data.py
+++ CryptoCurrencies/management/commands/delete_data.py
@@ -1,15 +1,32 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
+from django.apps import apps
+from django.db import connection
+from django.conf import settings
 
 
 class Command(BaseCommand):
-    help = 'Cleans a specified database table'
+    help = 'Cleans a specified database table and reset its ID sequence'
 
     def add_arguments(self, parser):
-        parser.add_argument('table_name', type=str, help='Name of the table to delete')
+        parser.add_argument('app_label', type=str, help='Label of the app')
+        parser.add_argument('model_name', type=str, help='Name of the model to delete')
 
     def handle(self, *args, **kwargs):
-        table_name = kwargs['table_name']
+        app_label = kwargs['app_label']
+        model_name = kwargs['model_name']
 
-        table_name.objects.all().delete()
+        try:
+            model = apps.get_model(app_label, model_name)
+        except LookupError:
+            raise CommandError(f"Model '{app_label}.{model_name}' not found.")
 
-        self.stdout.write('All data has been deleted')
+        model.objects.all().delete()
+
+        if 'sqlite' in settings.DATABASES['default']['ENGINE']:
+            with connection.cursor() as cursor:
+                cursor.execute(f"DELETE FROM sqlite_sequence WHERE name='{model._meta.db_table}';")
+        elif 'postgresql' in settings.DATABASES['default']['ENGINE']:
+            with connection.cursor() as cursor:
+                cursor.execute(f"SELECT setval(pg_get_serial_sequence('{model._meta.db_table}', 'id'), 1, false);")
+
+        self.stdout.write(self.style.SUCCESS(f'All data from {app_label}.{model_name} has been deleted and ID sequence reset'))
