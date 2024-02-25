509d6573d25d38b348b6b49bc8db8af125d61dec
kTurek95 <k.turek1995@gmail.com>
2024-02-25 17:53:45 +0000

    Added load_dotenv

--- CurrencyExchange/settings.py
+++ CurrencyExchange/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 import django_heroku
 import dj_database_url
+from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+load_dotenv()
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 
