1354bfcb7004ffe74e82a46fb0d63c87fb69dfa3
kTurek95 <k.turek1995@gmail.com>
2024-02-25 18:06:24 +0000

    Implemented a new currency instance and currency_name functionality. This addition is responsible for fetching the user-selected value from the database and displaying its name.

--- main/views.py
+++ main/views.py
@@ -6,10 +6,14 @@ def currency_view(request):
     result = None
     currency = AvailableCurrency.objects.all().order_by('code')
     rate = None
+    currency_name = None
     
     if request.method == 'POST':
         selected_currency = request.POST.get('currency')
         amount = float(request.POST.get('amount'))
+
+        currency_instance = AvailableCurrency.objects.get(code=selected_currency)
+        currency_name = currency_instance.name
         
         rate = CurrencyExchangeRate.objects.filter(currency__code=selected_currency).first()
         result = round(float(rate.rate_to_usd) * amount, 2)
@@ -17,6 +21,7 @@ def currency_view(request):
     context = {
         'currency_view': currency,
         'result': result,
-        'currency_rate': round(float(rate.rate_to_usd), 2) if rate else None
+        'currency_rate': round(float(rate.rate_to_usd), 2) if rate else None,
+        'currency_name': currency_name
     }
     return render(request, 'main/about.html', context)
