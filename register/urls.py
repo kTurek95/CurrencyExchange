73f0f98f1469d7f61f83eb5d0d663260034145e7
kTurek95 <k.turek1995@gmail.com>
2024-03-01 09:55:18 +0000

    Added path for activate_account function in views module

--- register/urls.py
+++ register/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from main.views import currency_view
-from .views import register
+from .views import register, activate_account
 
 app_name = 'register'
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('activate/<uidb64>/<token>/', activate_account, name='activate_account'),
     path('', currency_view, name='home')
 ]
