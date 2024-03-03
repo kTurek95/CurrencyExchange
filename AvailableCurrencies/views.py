f561a4e82a87b7d82a6687e2ed6491664e0f92ce
kTurek95 <k.turek1995@gmail.com>
2024-03-03 20:20:07 +0000

    Deleted unused 'login_view' function

--- AvailableCurrencies/views.py
+++ AvailableCurrencies/views.py
@@ -95,23 +95,6 @@ def currencies_rate(request):
     context = {'currencies_rate': rates_list}
     return render(request, 'AvailableCurrencies/currencies_rate.html', context)
 
-
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect(reverse('main:hello'))
-    else:
-        form = AuthenticationForm()
-
-    return render(request, 'registration/login.html', {'form': form})
-
-
 @login_required()
 def compare_previous_day_rate(request):
     today = date.today()
