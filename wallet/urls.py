42d5c3df81c2cae5f62a1ac0e8037bfb8ee2cca8
kTurek95 <k.turek1995@gmail.com>
2024-03-10 18:37:34 +0000

    Fixed minor errors in URL configurations
    This commit addresses and corrects small issues found in the URL configurations
    of the project.

--- wallet/urls.py
+++ wallet/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-# from .views import wallet, wallet_balance, wallet_selection, wallet_transaction
-from .views import UserWallet as Uw
-
+from .views import wallet, wallet_balance, wallet_selection, wallet_transaction
 app_name = 'wallet'
 urlpatterns = [
-    path('', Uw.create_wallet, name='info'),
-    path('balance/', Uw.wallet_balance, name='balance'),
-    path('selection/', Uw.wallet_selection, name='transactions'),
-    path('transacion/', Uw.wallet_transaction, name='transaction')
+    path('', wallet, name='info'),
+    path('balance/', wallet_balance, name='balance'),
+    path('selection/', wallet_selection, name='transactions'),
+    path('transacion/', wallet_transaction, name='transaction')
 ]
