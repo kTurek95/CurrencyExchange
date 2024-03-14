7d12ddd1c9ca62a4aaaaa52203e726441fa7678d
kTurek95 <k.turek1995@gmail.com>
2024-03-14 22:43:12 +0000

    Add docstrings and make Pylint recommended corrections
    This commit adds docstrings to all functions in the crypto currency handling module to improve code readability and maintainability. Also, various corrections have been made according to Pylint's recommendations to enhance code quality and adhere to Python best practices.

--- CryptoCurrencies/views.py
+++ CryptoCurrencies/views.py
@@ -1,22 +1,74 @@
+"""
+This module contains the views related to the CryptoTokenCurrency model in the Django application.
+
+The views in this module are responsible for
+handling the presentation layer of the crypto token currencies.
+It includes views for listing all the available crypto tokens
+and for displaying the details of a specific crypto token.
+These views interact with the CryptoTokenCurrency model to retrieve data from the database,
+and they render the appropriate templates with context data.
+
+Functions:
+    crypto_token(request): Renders a list of all crypto tokens with pagination.
+    crypto_token_details(request, crypto_id): Renders the details of a specific crypto token.
+"""
+
+
 from django.shortcuts import render
 from django.core.paginator import Paginator
-from .models import CryptoTokenCurrency
 from django.contrib.auth.decorators import login_required
+from .models import CryptoTokenCurrency
 
 
 def crypto_token(request):
+    """
+    View for listing all available crypto tokens.
+
+    This view retrieves all crypto tokens from the database, orders them by their code,
+    and paginates them with a fixed number of tokens per page
+    (300 tokens per page).
+    The view also handles the request to get a specific page of tokens.
+    It then passes the paginated tokens and menu context
+    to the crypto-token-list.html template for rendering.
+
+    Args:
+    request : HttpRequest
+        The incoming request object from the user.
+
+    Returns:
+    HttpResponse
+        Rendered web page with the list of crypto tokens and pagination controls.
+    """
     crypto = CryptoTokenCurrency.objects.all().order_by('code')
     paginator = Paginator(crypto, 300)
     page_number = request.GET.get('page')
-    crypto_token = paginator.get_page(page_number)
+    crypto_tokens = paginator.get_page(page_number)
     context = {
-        'crypto_token': crypto_token,
+        'crypto_token': crypto_tokens,
         'active_menu': 'Available Crypto/Tokens'}
     return render(request, 'CryptoCurrencies/crypto-token-list.html', context)
 
 
 @ login_required
 def crypto_token_details(request, crypto_id: int):
+    """
+    View for displaying the details of a specific crypto token.
+
+    This view fetches the details of a specific crypto token identified by the `crypto_id`.
+    It ensures that the user is logged in before accessing this view. If the token
+    exists, its details along with the menu context are passed to the
+    crypto-token-details.html template for rendering.
+
+    Args:
+    request : HttpRequest
+        The incoming request object from the user.
+    crypto_id : int
+        The unique identifier of the crypto token whose details are to be retrieved.
+
+    Returns:
+    HttpResponse
+        Rendered web page with the details of the specified crypto token.
+    """
     crypto_details = CryptoTokenCurrency.objects.get(pk=crypto_id)
     context = {
         'crypto_token_details': crypto_details,
