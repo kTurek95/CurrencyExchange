2f68e464580ca5e1991476e682b7d1d14de55e6e
kTurek95 <k.turek1995@gmail.com>
2024-04-09 12:26:39 +0100

    Extend wallet view function to handle 'make transaction' and 'show transaction' choices
    - Added functionality to differentiate between 'make transaction' and 'show transaction' actions in the wallet view function.
    - Implemented use of request.session to pass the selected wallet ID to the subsequent function for transaction processing. This ensures continuity and context preservation when navigating to the transaction execution stage.

--- wallet/views.py
+++ wallet/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from wallet.forms import WalletTransactionForm, SubmitWalletTransactionForm
 from .models import Wallet, Transaction
 from django.contrib import messages
-from AvailableCurrencies.models import AvailableCurrency
-from CryptoCurrencies.models import CryptoTokenCurrency
+from AvailableCurrencies.models import AvailableCurrency, CurrencyExchangeRate
+from CryptoCurrencies.models import CryptoTokenCurrency, CryptoTokenRate
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils import timezone
+from decimal import Decimal
 
 
 def wallet(request):
@@ -78,29 +83,64 @@ def wallet_balance(request):
     - HttpResponse: Renders the account balance page, showing the balance of each wallet
       for the authenticated user.
     """
+    currency_amount = None
+    crypto_amount = None
     if request.user.is_authenticated:
         user_id = request.user.id
         accounts = Wallet.objects.filter(user_id=user_id)
+        for account in accounts:
+            if account.currency_id is not None:
+                account_currency_id = account.currency_id
 
-        return render(request, 'wallet/account_balance.html', {
-            'accounts': accounts
-        })
+                # przeliczenie wartości portfela na USD
+                chosen_currency_rate = CurrencyExchangeRate.objects.filter(currency_id=account_currency_id).order_by(
+                    'api_date_updated').first()
+                currency_rate_in_usd = str(chosen_currency_rate).split('-')[-1].lstrip()
+                currency_amount = "{:.2f}".format(float(currency_rate_in_usd) * float(account.balance))
+
+            elif account.crypto_currency_id is not None:
+                account_crypto_currency_id = account.crypto_currency_id
+
+                # przeliczenie wartości portfela na USD
+                chosen_crypto_rate = CryptoTokenRate.objects.filter(token_id=account_crypto_currency_id).order_by(
+                    '-date').first()
+                crypto_rate_in_usd = str(chosen_crypto_rate).split(':')[-1].lstrip()
+                crypto_amount = "{:.2f}".format(float(crypto_rate_in_usd) * float(account.balance))
+
+    return render(request, 'wallet/account_balance.html', {
+        'accounts': accounts,
+        'currency_amount': currency_amount,
+        'crypto_amount': crypto_amount
+    })
 
 
 def wallet_selection(request):
     """
-    Allow the user to select a wallet and view its transaction history.
+    This view function handles different wallet-related actions for authenticated users. It retrieves
+    and displays the user's wallets, and based on the request method (GET or POST), it either displays
+    wallet information or processes wallet transactions.
 
-    This view displays all wallets associated with the authenticated user and allows
-    them to select one. Upon selection, the transaction history of the chosen wallet
-    is displayed, differentiated between standard currency and cryptocurrency.
+    For GET requests, the function simply returns the user's wallets.
+
+    For POST requests, it processes two types of actions:
+    - 'show_transaction': Displays transactions for a selected wallet. The transaction display
+      varies based on whether the wallet is a currency wallet or a crypto wallet.
+    - 'make_transaction': Redirects to a page for making transactions.
 
     Args:
-    - request (HttpRequest): The HTTP request object containing user data and method type.
+    - request: HttpRequest object containing metadata and information about the request
+      (such as the method type, POST data, and session information).
 
     Returns:
-    - HttpResponse: Renders the wallet selection page with a list of user's wallets and,
-      upon selection, displays the transaction history for the selected wallet.
+    - HttpResponse: Renders the appropriate template with context data based on the user's
+      request and actions. Possible templates rendered are:
+        - 'wallet/currency_history.html' for showing currency wallet transactions.
+        - 'wallet/crypto_history.html' for showing crypto wallet transactions.
+        - 'wallet/wallet_selection.html' for selecting a wallet or showing errors.
+
+    Raises:
+    - ValidationError: If a non-integer wallet ID is selected.
+    - Any database-related exceptions if the wallet or transaction objects cannot be retrieved.
     """
     context = {}
     if request.user.is_authenticated:
@@ -109,17 +149,22 @@ def wallet_selection(request):
         context['user_wallets'] = user_wallets
 
         if request.method == "POST":
+            action = request.POST.get('action')
             selected_wallet_id = request.POST.get('selected_wallet')
+            request.session['selected_wallet_id'] = selected_wallet_id
             if selected_wallet_id.isdigit():
-                selected_wallet = Wallet.objects.get(id=selected_wallet_id)
-                transactions = Transaction.objects.filter(wallet=selected_wallet)
-                context['transactions'] = transactions
-                if selected_wallet.currency:
-                    return render(request, 'wallet/currency_history.html', context)
-                elif selected_wallet.crypto_currency:
-                    return render(request, 'wallet/crypto_history.html', context)
-                else:
-                    messages.error(request, 'Please choose a wallet type')
+                if action == 'show_transaction':
+                    selected_wallet = Wallet.objects.get(id=selected_wallet_id)
+                    transactions = Transaction.objects.filter(wallet=selected_wallet)
+                    context['transactions'] = transactions
+                    if selected_wallet.currency:
+                        return render(request, 'wallet/currency_history.html', context)
+                    elif selected_wallet.crypto_currency:
+                        return render(request, 'wallet/crypto_history.html', context)
+                    else:
+                        messages.error(request, 'Please choose a wallet type')
+                elif action == 'make_transaction':
+                    return HttpResponseRedirect(reverse('wallet:transaction'))
             else:
                 context['error'] = 'Please select a valid wallet.'
 
