5d329fc3d486f8753cc7e05148896c22d891255a
kTurek95 <k.turek1995@gmail.com>
2024-05-11 21:14:08 +0100

    Added view that integrates with the transaction finalization.html template
    Implemented the  view function that works in conjunction with the previously added  template. This function handles transaction processes for both cryptocurrencies and traditional currencies, based on user data stored in the session. It performs fetching of current exchange rates from the database, calculating transaction values in USD and the wallet's currency, and updating the wallet balance upon transaction approval. The view is also responsible for form initialization and submission, checking user authentication and sufficient funds, and providing appropriate feedback messages.
    
    Additionally, added URLs for the views  and  to enable routing and accessibility within the application.

--- wallet/urls.py
+++ wallet/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import wallet, wallet_balance, wallet_selection, wallet_transaction
+from .views import wallet, wallet_balance, wallet_selection, prepare_and_process_wallet_transaction, wallet_transaction
 app_name = 'wallet'
 urlpatterns = [
     path('', wallet, name='info'),
     path('balance/', wallet_balance, name='balance'),
     path('selection/', wallet_selection, name='transactions'),
-    path('transacion/', wallet_transaction, name='transaction')
+    path('transacion/', prepare_and_process_wallet_transaction, name='transaction'),
+    path('finalization/', wallet_transaction, name='wallet_transaction')
 ]
--- wallet/views.py
+++ wallet/views.py
@@ -255,3 +255,230 @@ def prepare_and_process_wallet_transaction(request):
         'form': form,
     }
     return render(request, 'wallet/make_transaction.html', context)
+
+
+def wallet_transaction(request):
+    """
+    Handles the transaction processes within a user's wallet, supporting both cryptocurrency and standard currency transactions based on the user's selection. The function determines the type of wallet (crypto or currency), fetches the latest exchange rate for the selected type, calculates transaction values in both the wallet's currency and USD, and updates the wallet balance if the transaction is approved.
+
+    Parameters:
+    - request (HttpRequest): The HTTP request object containing session data like selected currency, transaction amount, and other necessary information.
+
+    Returns:
+    - HttpResponse: Renders the 'transaction finalization.html' template with the context containing transaction details and updates based on the outcome (success or error).
+
+    The function also handles form initialization and submission, updates the wallet's balance upon successful transactions, and logs transaction details. If a user is authenticated and the POST method is used, the function attempts to perform the transaction and update the database accordingly. It provides feedback to the user via messages indicating the success or failure of the transaction.
+    """
+
+    wallet_instance_id = request.session.get('wallet_instance_id')
+    wallet_instance = Wallet.objects.get(id=wallet_instance_id)
+
+    if str(wallet_instance) == 'Crypto wallet':
+        crypto = request.session.get('selected_crypto')
+        current_wallet_crypto_id = CryptoTokenCurrency.objects.get(id=wallet_instance.crypto_currency_id).id
+
+        current_wallet_crypto_id_rate = (
+            CryptoTokenRate.objects.filter(
+                token_id=current_wallet_crypto_id).order_by('-date').first())
+
+        current_wallet_crypto_id_rate_format = str(current_wallet_crypto_id_rate).split(':')[-1].lstrip()
+
+    elif str(wallet_instance) == 'Currency wallet':
+        currency = request.session.get('selected_currency')
+        current_wallet_currency_id = (
+            AvailableCurrency.objects.get(
+                code=str(wallet_instance.currency).split('-')[0].rstrip()))
+
+        current_wallet_currency_id_rate = (
+            CurrencyExchangeRate.objects.filter(
+                currency_id=current_wallet_currency_id).order_by('api_date_updated').first())
+
+        current_wallet_currency_id_rate_format = str(current_wallet_currency_id_rate).split("-")[-1].lstrip()
+
+    wallet_instance_currency_id = Wallet.objects.get(id=wallet_instance_id).currency_id
+    wallet_instance_crypto_currency_id = Wallet.objects.get(id=wallet_instance_id).crypto_currency_id
+
+    currency_wallet_instance_balance = Wallet.objects.get(id=wallet_instance_id).balance
+    crypto_wallet_instance_balance = Wallet.objects.get(id=wallet_instance_id).balance
+
+    currency_rate = None
+    crypto_rate = None
+    total_crypto_transaction_value_in_usd = None
+    total_currency_transaction_value_in_usd = None
+    dane_formularza_currency = request.session.get('currency_form_data')
+    dane_formularza_crypto = request.session.get('crypto_form_data')
+    amount = request.session.get('amount')
+
+    # jeśli użytkownik wybierze zwykłą walutę
+    # if the user selects a regular currency
+    if dane_formularza_currency is not None:
+        # pobranie informacji na temat waluty w portfelu i przeliczenie jej wartosci na usd
+        # retrieve information about the wallet's currency and convert its value to USD
+        currency_rate_from_database = (
+            CurrencyExchangeRate.objects.filter(
+                currency_id=wallet_instance_currency_id).order_by('api_date_updated').first().rate_to_usd)
+        currency_wallet_balance_in_usd = (
+            "{:.2f}".format(float(currency_rate_from_database) * float(currency_wallet_instance_balance)))
+
+        # obliczenie całkowitej sumy transakcji w usd
+        # calculate the total transaction amount in USD
+        currency_rate = (
+            CurrencyExchangeRate.objects.filter
+            (currency_id=dane_formularza_currency).order_by('api_date_updated').first()).rate_to_usd
+        currency_rate_value_to_usd = "{:.2f}".format(1 / float(currency_rate))
+        total_currency_transaction_value_in_usd = "{:.2f}".format(amount * float(currency_rate_value_to_usd))
+
+        # obliczenie sumy transakcji w walucie portfela
+        # calculate the transaction amount in the wallet's currency
+        transaction_amount_in_currency_wallet = "{:.2f}".format(
+            float(total_currency_transaction_value_in_usd) / float(current_wallet_currency_id_rate_format)\
+        )
+
+        # dane do formularza
+        # data for the form
+        initial_data = {
+            'wallet_balance': wallet_instance.balance,
+            'wallet_balance_in_usd': currency_wallet_balance_in_usd,
+            'selected_currency': currency,
+            'currency_amount': amount,
+            'currency_rate': currency_rate_value_to_usd,
+            'transaction_sum_in_usd': total_currency_transaction_value_in_usd,
+            'transaction_sum': transaction_amount_in_currency_wallet
+        }
+
+        balance_after_transaction = (
+            "{:.2f}".format(
+                float(currency_wallet_balance_in_usd) - float(total_currency_transaction_value_in_usd), 2))
+        form = SubmitWalletTransactionForm(initial=initial_data)
+
+        if request.method == 'POST' and request.user.is_authenticated:
+            if float(currency_wallet_balance_in_usd) >= float(total_currency_transaction_value_in_usd):
+                # wartość transakcji w walucie portfela (typ decimal)
+                # transaction value in the wallet's currency (decimal type)
+
+                decimal_value = "{:.2f}".format(Decimal(transaction_amount_in_currency_wallet))
+                wallet_instance.balance -= Decimal(decimal_value)
+                wallet_instance.save()
+
+                # tworzenie transakcji
+                # creating the transaction
+                new_transaction = Transaction(
+                    user_id=request.user,
+                    wallet=wallet_instance,
+                    amount=amount,
+                    transaction_type=Transaction.BUY,
+                    purchased_currency=currency,
+                    status=Transaction.COMPLETED,
+                    date=timezone.now(),
+                    value_in_USD=total_currency_transaction_value_in_usd,
+                    balance_after_transaction=balance_after_transaction
+                )
+
+                new_transaction.save()
+                messages.success(request, 'The transaction was successful')
+                return HttpResponseRedirect(reverse('main:currency'))
+            else:
+                new_transaction = Transaction(
+                    wallet=wallet_instance,
+                    amount=amount,
+                    transaction_type=Transaction.BUY,
+                    purchased_currency=currency,
+                    status=Transaction.REJECTED,
+                    date=timezone.now(),
+                    value_in_USD=total_currency_transaction_value_in_usd,
+                    balance_after_transaction=currency_wallet_balance_in_usd
+                )
+                new_transaction.save()
+                messages.error(request, 'You do not have sufficient funds in your account')
+
+    # jeśli użytkownik wybierze kryptowalutę
+    # if the user selects a cryptocurrency
+    elif dane_formularza_crypto is not None:
+
+        # pobranie kursu kryptowaluty w portfelu z bazy danych
+        # retrieve the cryptocurrency rate from the database for the wallet
+        crypto_rate_from_database =\
+            CryptoTokenRate.objects.filter(
+                token_id=wallet_instance_crypto_currency_id).order_by('-date').first().rate_to_usd
+
+        # zamiana waluty portfela na usd
+        # convert the wallet's currency to USD
+        crypto_wallet_balance_in_usd = (
+            "{:.2f}".format(float(crypto_rate_from_database) * float(crypto_wallet_instance_balance)))
+
+        # obliczenie całkowitej sumy transakcji w usd
+        # calculate the total transaction amount in USD
+        crypto_rate = (
+            CryptoTokenRate.objects.filter
+            (token_id=dane_formularza_crypto).order_by('-date').first()).rate_to_usd
+        total_crypto_transaction_value_in_usd = "{:.2f}".format(amount * float(crypto_rate))
+
+        # obliczenie sumy transakcji w walucie portfela
+        # calculate the transaction amount in the wallet's currency
+        transaction_amount_in_crypto_wallet = "{:.2f}".format(
+            float(total_crypto_transaction_value_in_usd) / float(current_wallet_crypto_id_rate_format)
+        )
+
+        # dane do formularza
+        # data for the form
+        initial_data = {
+            'wallet_balance': wallet_instance.balance,
+            'wallet_balance_in_usd': crypto_wallet_balance_in_usd,
+            'selected_currency': crypto,
+            'currency_amount': amount,
+            'currency_rate': crypto_rate,
+            'transaction_sum_in_usd': total_crypto_transaction_value_in_usd,
+            'transaction_sum': transaction_amount_in_crypto_wallet
+        }
+
+        balance_after_transaction =\
+            "{:.2f}".format(float(crypto_wallet_balance_in_usd) - float(total_crypto_transaction_value_in_usd))
+        form = SubmitWalletTransactionForm(initial=initial_data)
+
+        if request.method == "POST" and request.user.is_authenticated:
+            if float(crypto_wallet_balance_in_usd) >= float(total_crypto_transaction_value_in_usd):
+                # wartość transakcji w walucie portfela (typ decimal)
+                # transaction value in the wallet's currency (decimal type)
+                decimal_value = "{:.2f}".format(Decimal(transaction_amount_in_crypto_wallet))
+                wallet_instance.balance -= Decimal(decimal_value)
+                wallet_instance.save()
+
+                # tworzenie transakcji
+                # creating the transaction
+                new_transaction = Transaction(
+                    user_id=request.user,
+                    wallet=wallet_instance,
+                    amount=amount,
+                    transaction_type=Transaction.BUY,
+                    purchased_currency=crypto,
+                    status=Transaction.COMPLETED,
+                    date=timezone.now(),
+                    value_in_USD=total_crypto_transaction_value_in_usd,
+                    balance_after_transaction=balance_after_transaction
+                )
+
+                new_transaction.save()
+                messages.success(request, 'The transaction was successful')
+                return HttpResponseRedirect(reverse('main:currency'))
+            else:
+                new_transaction = Transaction(
+                    wallet=wallet_instance,
+                    amount=amount,
+                    transaction_type=Transaction.BUY,
+                    purchased_currency=crypto,
+                    status=Transaction.REJECTED,
+                    date=timezone.now(),
+                    value_in_USD=total_crypto_transaction_value_in_usd,
+                    balance_after_transaction=crypto_wallet_balance_in_usd
+                )
+                new_transaction.save()
+                messages.error(request, 'You do not have sufficient funds in your account')
+    context = {
+        'currency_rate': currency_rate,
+        'crypto_rate': crypto_rate,
+        'total_crypto_transaction_value': total_crypto_transaction_value_in_usd,
+        'total_currency_transaction_value': total_currency_transaction_value_in_usd,
+        'form': form
+
+    }
+    return render(request, 'wallet/transaction finalization.html', context)
