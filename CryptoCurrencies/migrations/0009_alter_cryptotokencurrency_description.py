455793948fb4d33c6badf2f7dedff66fc7a5890b
kTurek95 <k.turek1995@gmail.com>
2024-08-21 08:58:27 +0200

    Changed chars limit in migrations 009 to 5000

--- CryptoCurrencies/migrations/0009_alter_cryptotokencurrency_description.py
+++ CryptoCurrencies/migrations/0009_alter_cryptotokencurrency_description.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='cryptotokencurrency',
             name='description',
-            field=models.CharField(max_length=3000, null=True),
+            field=models.CharField(max_length=5000, null=True),
         ),
     ]
